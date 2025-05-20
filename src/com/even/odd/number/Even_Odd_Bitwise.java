64ca97e9c8c47979b7eb89f3656bb7f5999dbcc8
Aakash Gupta <aakash408408@gmail.com>
2025-05-20 19:10:41 +0530

    Logic Building Program

--- src/com/even/odd/number/Even_Odd_Bitwise.java
+++ src/com/even/odd/number/Even_Odd_Bitwise.java
@@ -2,4 +2,14 @@ package com.even.odd.number;
 
 public class Even_Odd_Bitwise {
 
+	public static void main(String[] args) {
+
+		int number = 4;
+
+		if ((number & 1) == 1) {
+			System.out.println("Number is ODD");
+		} else {
+			System.out.println("Number is EVEN");
+		}
+	}
 }
